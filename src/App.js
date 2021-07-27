2e63a01bbe7ec28ff0f8926cc709444b08e2d2dc
josego <josego85@gmail.com>
2021-07-26 21:28:28 -0400

    Refactoring code.

--- src/App.js
+++ src/App.js
@@ -6,29 +6,23 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Sidebar from './Components/Sidebar';
 import { Navbar } from 'react-bootstrap';
 import Products from './Components/Products'; 
-
 import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
-    
     <Router>
       <div className="App">
         <Navbar bg="myColor" variant="dark">
-  
           <Navbar.Brand href="#">ProductosPY</Navbar.Brand>
           <Navbar.Toggle />
           <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text>
-           Signed in as: <a href="#">Ana Rivas</a>
-          </Navbar.Text>
-        </Navbar.Collapse>
-        
+            <Navbar.Text>
+              Signed in as: <a href="#">Ana Rivas</a>
+            </Navbar.Text>
+          </Navbar.Collapse>
         </Navbar>
         <Sidebar></Sidebar>
         <Products></Products>
-        
-
       </div>
     </Router>
   );
--- src/Components/Products.js
+++ src/Components/Products.js
@@ -2,18 +2,17 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Card, Button } from 'react-bootstrap';
 import { BrowserRouter as Router } from 'react-router-dom';
-import SidebarProducts from './SidebarProducts'; 
+import SidebarProducts from './SidebarProducts';
+
 function Products() {
     return (
         <Router>
-        
-        <div class="row no-gutters">
-            <div class="col-6 col-md-4">
-                <SidebarProducts></SidebarProducts>
-            </div>
-                
+            <div class="row no-gutters">
+                {/* <div class="col-6 col-md-4">
+                    <SidebarProducts></SidebarProducts>
+                </div> */}
                 <div class="col-12 col-md-8">
-                    <div class="card-deck" w-75> 
+                    <div class="card-deck"> 
                         <div class="card" style={{ width: '20rem' }}>
                             <img class="card-img-top" src="https://cdn.shopify.com/s/files/1/0501/3218/9340/products/IMG-20210129-WA0031.jpg?v=1611964113" alt="Card image cap"/>
                             <div class="card-body">
@@ -22,6 +21,7 @@ function Products() {
                                 
                             </div>
                         </div>
+
                         <div class="card" style={{ width: '20rem' }}>
                             <img class="card-img-top" src="https://cdn.shopify.com/s/files/1/0501/3218/9340/products/IMG-20210129-WA0031.jpg?v=1611964113" alt="Card image cap"/>
                             <div class="card-body">
@@ -39,17 +39,11 @@ function Products() {
                                 
                             </div>
                         </div>
-
-                        
-                       
                     </div>
                 </div>
             </div>
-                   
         </Router> 
     )
 }
 
-
-export default Products 
-
+export default Products;
